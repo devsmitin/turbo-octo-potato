218f7bbd879ffce21249bb47eeb1be661abc0d44
Smit <aquarocker20@gmail.com>
2022-02-04 23:49:55 +0530

    added layout ✅

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import Header from "./components/Header";
+import Layout from "./components/Layout";
 import Hero from "./components/Hero1";
 import CollectionListSection from "./components/CollectionListSection";
 import ProductListSection from "./components/ProductListSection";
 import Newsletter from "./components/Newsletter1";
-import Footer from "./components/Footer1";
 
 function App() {
   return (
-    <>
-      <Header />
+    <Layout>
       <Hero />
       <CollectionListSection />
       <ProductListSection />
       <Newsletter />
-      <Footer />
-    </>
+    </Layout>
   );
 }
 
